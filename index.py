18074e00b0449aef6fc25f533115f226949c134f
mishrariy <riyamishra1018@gmail.com>
2022-03-08 09:34:06 +0530

    Task1: reveseStringFunction is defined

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
 print("This is my index.py file")
 
 print("This is my index.py file2")
+
+#Task1: string
+s = "IamLearningInSOU"
+
+def reversStringFunc(a):
+    "reverse function logic"
+    return a
+
+print(reversStringFunc(s))
